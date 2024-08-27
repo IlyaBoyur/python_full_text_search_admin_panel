cc4e37f06742fc57180eac29d948c9ef86860435
Ilya Boyur <i.boyur@idaproject.com>
2024-08-27 11:29:34 +0300

    Update settings

--- sqlite_to_postgres/settings.py
+++ sqlite_to_postgres/settings.py
@@ -4,5 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLITE_DB_NAME = os.environ.get("SQLITE_PATH", "db.sqlite")
-BULK_SIZE = int(os.environ.get("BULK_SIZE", "1"))
+
+SQLITE_DATABASE = os.environ.get("SQLITE_DATABASE", "db.sqlite")
+
+POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "postgres")
+POSTGRES_DB = os.environ.get("POSTGRES_DB", "postgres")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "127.0.0.1")
+POSTGRES_PORT = int(os.environ.get("POSTGRES_PORT", 5432))
+POSTGRES_INIT = os.environ.get("POSTGRES_INIT", "")
+
+BULK_SIZE = int(os.environ.get("BULK_SIZE", 1))
