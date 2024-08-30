1669eea1d159e6cb262f596e7e84d199b1e0bb70
Ilya Boyur <i.boyur@idaproject.com>
2024-08-30 07:23:56 +0300

    Fix BASE_DIR

--- movies_admin/config/settings/base.py
+++ movies_admin/config/settings/base.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
